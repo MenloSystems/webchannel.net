18c9ad7460c8dd43c0f797f928061118ca7909fb
Arno Rehn <a.rehn@menlosystems.com>
2020-08-18 16:29:02 +0200

    Add an async WebSocket based transport

--- src/Transports.cs
+++ src/Transports.cs
@@ -7,8 +7,10 @@
 using System;
 using System.IO;
 using System.Net.Sockets;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Collections;
 
 namespace QWebChannel
@@ -201,6 +203,132 @@ namespace QWebChannel
                     OnMessage(this, message);
                 }
             }
+        }
+    }
+
+    public class WebChannelWebSocketTransport : IWebChannelTransport, IDisposable
+    {
+        ClientWebSocket sock;
+
+        public event Action<object, byte[]> OnMessage;
+        public event EventHandler OnDisconnected;
+        public event EventHandler<UnhandledExceptionEventArgs> OnError;
+
+        public bool Connected { get { return sock.State == WebSocketState.Open; } }
+        public ClientWebSocket ClientWebSocket { get { return sock; } }
+
+        public WebChannelWebSocketTransport() {
+            sock = new ClientWebSocket();
+        }
+
+        public async Task Connect(Uri uri)
+        {
+            await sock.ConnectAsync(uri, CancellationToken.None);
+        }
+
+        // https://stackoverflow.com/a/23784968 - because .NET WebSockets don't
+        // do message handling.
+
+        private struct WebSocketMessage
+        {
+            public WebSocketReceiveResult result;
+            public byte[] data;
+        }
+        async Task<WebSocketMessage> ReceiveMessage()
+        {
+            ArraySegment<Byte> buffer = new ArraySegment<byte>(new Byte[8192]);
+            WebSocketMessage msg = new WebSocketMessage();
+
+            using (var ms = new MemoryStream()) {
+                do {
+                    try {
+                        msg.result = await sock.ReceiveAsync(buffer, CancellationToken.None);
+                    } catch(WebSocketException e) {
+                        if (OnError != null) {
+                            OnError(this, new UnhandledExceptionEventArgs(e, false));
+                        }
+                        await Disconnect(e.ToString());
+                        return msg;
+                    }
+                    if (msg.result.MessageType == WebSocketMessageType.Close) {
+                        await Disconnect(msg.result.CloseStatusDescription);
+                        return msg;
+                    }
+                    ms.Write(buffer.Array, buffer.Offset, msg.result.Count);
+                }
+                while (!msg.result.EndOfMessage);
+
+                ms.Seek(0, SeekOrigin.Begin);
+
+                msg.data = ms.GetBuffer();
+                return msg;
+            }
+        }
+
+        void ProcessMessage(WebSocketMessage msg)
+        {
+            if (msg.result != null && msg.result.MessageType == WebSocketMessageType.Text) {
+                OnMessage(this, msg.data);
+            }
+        }
+
+        Task<WebSocketMessage> recvMsgTask = Task.FromResult(new WebSocketMessage());
+
+        public void Pump()
+        {
+            if (recvMsgTask.IsCompleted) {
+                ProcessMessage(recvMsgTask.Result);
+                recvMsgTask = ReceiveMessage();
+            }
+        }
+
+        public async Task ProcessNextMessageAsync()
+        {
+            await recvMsgTask;
+            Pump();
+        }
+
+        public async Task ProcessMessagesAsync()
+        {
+            while (Connected) {
+                await ProcessNextMessageAsync();
+            }
+        }
+
+        public void Dispose()
+        {
+            Disconnect().Wait();
+            if (sock != null) {
+                ((IDisposable) sock).Dispose();
+            }
+        }
+
+        public async Task Disconnect()
+        {
+            await Disconnect("Closed by Disconnect()");
+        }
+
+        async Task Disconnect(string reason)
+        {
+            if (sock != null) {
+                if (sock.State == WebSocketState.Open || sock.State == WebSocketState.Connecting) {
+                    await sock.CloseAsync(WebSocketCloseStatus.NormalClosure, reason, CancellationToken.None);
+                }
+                if (OnDisconnected != null) {
+                    OnDisconnected(this, new EventArgs());
+                }
+            }
+        }
+
+
+        public void Send(string msg)
+        {
+            Send(Encoding.UTF8.GetBytes(msg));
+        }
+
+        public void Send(byte[] msg)
+        {
+            sock.SendAsync(new ArraySegment<byte>(msg), WebSocketMessageType.Text, true, CancellationToken.None).Wait();
         }
     }
 }
